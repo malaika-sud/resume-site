7f5e3d3812dcc0fa091d81b39dfe7272e77353e9
Malaika Sud <malaikasud@Malaikas-MacBook-Pro.local>
2022-12-27 19:17:30 -0800

    added project section, will revamp later when additional projects are completed

--- resume-app/src/App.js
+++ resume-app/src/App.js
@@ -1,10 +1,10 @@
-// import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Experience } from "./components/Experience";
+import { Projects } from "./components/Projects";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Banner/>
       <Skills/>
       <Experience/>
+      <Projects/>
     </div>
   );
 }
---
+++ resume-app/src/components/Projects.js
@@ -0,0 +1,46 @@
+import { Container, Row, Col } from "react-bootstrap"
+import world from '../assets/img/world.png';
+import foodge from "../assets/img/foodge.png";
+import boops from "../assets/img/bg.png"
+
+export const Projects = () => {
+
+
+    return (
+        <section className="projects" id="projects">
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="projects-bx">
+                        <h2> My Projects </h2>
+                        <p> <br/>A section to host some of my personal projects &amp; showcase my interests in coding! My skills, as listed above, put to use.</p>
+                        
+                        <div className="proj1">
+                        <a href="https://people.ucsc.edu/~msud/" target="_blank"><img src={world} alt="" /></a>
+                    
+                            <div className="txt">
+                                A collection of the projects I created for CSE 160 Computer Graphics, Fall '22. Click on each ASG folder &amp; then the related .html file to see the final product displayed on your browser! Projects created using HTML, CSS, JS, and for ASG5 utilizing the Three.js library. <br/>
+                            </div>
+                
+                        </div>
+
+                        <div className="proj2">
+                        {/* CHANGE THE LINK LATER WHEN HOSTED ON A PLATFORM */}
+                        <a href="https://people.ucsc.edu/~msud/" target="_blank"><img src={foodge} alt="" /></a>
+                    
+                            <div className="txt2">
+                                A recipe generator website inspired by my college cooking skills (or lack thereof). I began this project to find a solution to the fact that I "never had anything to eat". Created utilizing the MealDB API, HTML, CSS, and JS.  <br/>
+                            </div>
+                
+                        </div>
+
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+            <img className="background-image-woop" src={boops}></img>
+        </section>
+    )
+
+}
